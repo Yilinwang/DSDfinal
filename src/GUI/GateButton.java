3c903d98a4f2592ae931d2c5c93aabe4a2223908
Yilinwang <linda.yilin@gmail.com>
2016-06-05 17:59:39 +0800

    primary version

--- src/GUI/GateButton.java
+++ src/GUI/GateButton.java
@@ -1,13 +1,17 @@
 package GUI;
 
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 
 import java.util.ArrayList;
 
 public class GateButton extends Button {
     private String type; // cmd or obj
     private String gate; // jkff, rsff, dff, tff, and, or, xor, nand, not
+    private String name;
     private ArrayList<GateButton> input;
+    private GraphicsContext gc;
 
     private void maintain(ArrayList<GateButton> queue) {
         if(queue.size() >= 2 && queue.get(queue.size() - 1).getType() == "obj") {
@@ -16,8 +20,17 @@ public class GateButton extends Button {
 
             if(second.getType() == "obj" && last.getGate() != "" && second.getGate() != "") {
                 // draw line!!!!
-                if(!last.isInputFull())
-                    last.setInput(second);
+                if(!last.isInputFull() && !(second.getGate() == "z")) {
+                    GridPane gridPane = (GridPane) last.getParent();
+                    int prevRowIndex = gridPane.getRowIndex(second);
+                    int lastRowIndex = gridPane.getRowIndex(last);
+                    int prevColIndex = gridPane.getColumnIndex(second);
+                    int lastColIndex = gridPane.getColumnIndex(last);
+                    if(prevRowIndex != lastRowIndex || prevColIndex != lastColIndex) {
+                        gc.strokeLine(70 + prevColIndex * 70, 25 + prevRowIndex * 40, 10 + lastColIndex * 70, 25 + lastRowIndex * 40);
+                        last.setInput(second);
+                    }
+                }
             }
             else if(second.getType() == "cmd" && last.getGate() == "")
                 last.setGate(second.getGate());
@@ -26,10 +39,11 @@ public class GateButton extends Button {
         }
     }
 
-    public GateButton(String _type, String _gate, ArrayList<GateButton> queue) {
+    public GateButton(String _type, String _gate, ArrayList<GateButton> queue, GraphicsContext _gc) {
         super(_gate);
         type = _type;
         gate = _gate;
+        gc = _gc;
         input = new ArrayList<>();
         super.setOnAction((event -> {
             queue.add(this);
@@ -37,14 +51,30 @@ public class GateButton extends Button {
         }));
     }
 
+    public void setName(String _name) {
+        name = _name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<GateButton> getInput() {
+        return input;
+    }
+
     public boolean isInputFull() {
         switch(gate) {
+            case "x":
+                return true;
             case "jkFF": case"rsFF":
-                if(input.size() == 2)
+                if(input.size() >= 2)
                     return true;
-            case "dFF": case "tFF": case"not":
-                if(input.size() == 1)
+                break;
+            case "dFF": case "tFF": case "not":case "z":
+                if(input.size() >= 1)
                     return true;
+                break;
         }
         return false;
     }
@@ -64,5 +94,6 @@ public class GateButton extends Button {
     public void setGate(String _gate) {
         gate = _gate;
         super.setText(gate);
+        super.setStyle("-fx-border-width: 1px");
     }
 }
--- src/GUI/reverseGUI.java
+++ src/GUI/reverseGUI.java
@@ -2,50 +2,151 @@ package GUI;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
-import java.util.Stack;
 
 public class reverseGUI extends Application {
+    private void submitFunc(GridPane gridPane) {
+        int count = 0;
+        for(Node tmp: gridPane.getChildren()) {
+            GateButton gate = (GateButton)tmp;
+            switch(gate.getGate()) {
+				case "jkFF":
+					System.out.printf("g%d = JKflipflop()\n", count);
+                    System.out.printf("state.addFlipFlop(g%d)\n", count);
+                    gate.setName("g" + String.valueOf(count));
+					count++;
+					break;
+				case "rsFF":
+					System.out.printf("g%d = RSflipflop()\n", count);
+                    System.out.printf("state.addFlipFlop(g%d)\n", count);
+                    gate.setName("g" + String.valueOf(count));
+					count++;
+					break;
+				case "dFF":
+					System.out.printf("g%d = Dflipflop()\n", count);
+                    System.out.printf("state.addFlipFlop(g%d)\n", count);
+                    gate.setName("g" + String.valueOf(count));
+                    count++;
+                    break;
+                case "tFF":
+                    System.out.printf("g%d = Tflipflop()\n", count);
+                    System.out.printf("state.addFlipFlop(g%d)\n", count);
+                    gate.setName("g" + String.valueOf(count));
+                    count++;
+                    break;
+                case "and":
+                    System.out.printf("g%d = andGate()\n", count);
+                    gate.setName("g" + String.valueOf(count));
+                    count++;
+                    break;
+                case "or":
+                    System.out.printf("g%d = orGate()\n", count);
+                    gate.setName("g" + String.valueOf(count));
+                    count++;
+                    break;
+                case "xor":
+                    System.out.printf("g%d = xorGate()\n", count);
+                    gate.setName("g" + String.valueOf(count));
+                    count++;
+                    break;
+                case "nand":
+                    System.out.printf("g%d = nandGate()\n", count);
+                    gate.setName("g" + String.valueOf(count));
+                    count++;
+                    break;
+                case "nor":
+                    System.out.printf("g%d = norGate()\n", count);
+                    gate.setName("g" + String.valueOf(count));
+                    count++;
+                    break;
+                case "not":
+                    System.out.printf("g%d = notGate()\n", count);
+                    gate.setName("g" + String.valueOf(count));
+                    count++;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        for(Node tmp: gridPane.getChildren()) {
+            GateButton gate = (GateButton)tmp;
+            if(gate.getType() == "obj") {
+                for(GateButton inputGate: gate.getInput())
+                    System.out.printf(gate.getName()+".set("+inputGate.getName()+")\n");
+            }
+        }
+    }
+
     public void start(Stage stage) {
         ArrayList<GateButton> queue = new ArrayList<>();
 
         BorderPane borderPane = new BorderPane();
-
+        Canvas canvas = new Canvas(780, 380);
         HBox hBox = new HBox();
-        hBox.setPadding(new Insets(10, 10, 10, 10));
-        hBox.setSpacing(10);
-        hBox.getChildren().add(new GateButton("cmd", "jkFF", queue));
-        hBox.getChildren().add(new GateButton("cmd", "rsFF", queue));
-        hBox.getChildren().add(new GateButton("cmd", "dFF", queue));
-        hBox.getChildren().add(new GateButton("cmd", "tFF", queue));
-        hBox.getChildren().add(new GateButton("cmd", "and", queue));
-        hBox.getChildren().add(new GateButton("cmd", "or", queue));
-        hBox.getChildren().add(new GateButton("cmd", "xor", queue));
-        hBox.getChildren().add(new GateButton("cmd", "nand", queue));
-        hBox.getChildren().add(new GateButton("cmd", "not", queue));
-        borderPane.setTop(hBox);
-
+        HBox bottom = new HBox();
         StackPane stackPane = new StackPane();
-        borderPane.setCenter(stackPane);
-
         GridPane gridPane = new GridPane();
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.setLineWidth(1);
+
+        hBox.setAlignment(Pos.CENTER);
+        hBox.setPadding(new Insets(10));
+        hBox.setSpacing(10);
+        hBox.getChildren().add(new GateButton("cmd", "jkFF", queue, gc));
+        hBox.getChildren().add(new GateButton("cmd", "rsFF", queue, gc));
+        hBox.getChildren().add(new GateButton("cmd", "dFF", queue, gc));
+        hBox.getChildren().add(new GateButton("cmd", "tFF", queue, gc));
+        hBox.getChildren().add(new GateButton("cmd", "and", queue, gc));
+        hBox.getChildren().add(new GateButton("cmd", "or", queue, gc));
+        hBox.getChildren().add(new GateButton("cmd", "xor", queue, gc));
+        hBox.getChildren().add(new GateButton("cmd", "nand", queue, gc));
+        hBox.getChildren().add(new GateButton("cmd", "not", queue, gc));
+
+        bottom.setAlignment(Pos.CENTER_RIGHT);
+        bottom.setPadding(new Insets(10));
+        Button submit = new Button("Submit!!!!!");
+        submit.setStyle("-fx-max-width: 120px");
+        submit.setOnAction((event -> {
+            submitFunc(gridPane);
+        }));
+        bottom.getChildren().add(submit);
+
         gridPane.setVgap(10);
         gridPane.setHgap(10);
         gridPane.setPadding(new Insets(10, 10, 10, 10));
-        for(int i = 0; i < 9; i++)
-            for(int j = 0; j < 9; j++)
-                gridPane.add(new GateButton("obj", "", queue), i, j);
+        GateButton x = new GateButton("obj", "x", queue, gc);
+        x.setName("x");
+        gridPane.add(x, 0, 4);
+        GateButton z = new GateButton("obj", "z", queue, gc);
+        z.setName("z");
+        gridPane.add(z, 10, 4);
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++) {
+                Button tmp = new GateButton("obj", "", queue, gc);
+                tmp.setStyle("-fx-border-width: 0px");
+                gridPane.add(tmp, i + 1, j);
+            }
+        }
+
+        stackPane.getChildren().add(canvas);
         stackPane.getChildren().add(gridPane);
 
-        Scene scene = new Scene(borderPane, 640, 380);
+        borderPane.setTop(hBox);
+        borderPane.setCenter(stackPane);
+        borderPane.setBottom(bottom);
+
+        Scene scene = new Scene(borderPane, 780, 480);
         scene.getStylesheets().add("GUI/style.css");
         stage.setScene(scene);
         stage.show();
